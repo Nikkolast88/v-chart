ed6a0b1ab781e3629ec5617d76b26e2a6894b71b
dengriguang <1971167057@qq.com>
2019-07-12 10:46:57 +0800

    demo finish

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,117 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <VeMap
+      ref="mapChart"
+      :settings="chartSettings"
+      :geo="chartGeo"
+      :series="chartSeries"
+      :data="mapData"
+    ></VeMap>
   </div>
 </template>
 
 <script>
+import VeMap from "v-charts/lib/map.common";
+import Axios from 'axios';
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  components: {
+    VeMap
+  },
   props: {
     msg: String
+  },
+  data() {
+    return{
+      chartTooltip: {},
+      mapData: {
+        columns: [],
+        rows: []
+      },
+      chartSettings: {},
+      chartSeries: {
+        type: 'map'
+      },
+      chartGeo: {},
+      longitude: '',
+      latitude: ''
+    }
+  },
+  created() {
+    this.initMap()
+  },
+  methods: {
+    initMap() {
+      Axios.get('./shenzhen.json').then(res => {
+        let shenzhen = res.data
+        this.chartTooltip = {
+          formatter: function(v) {
+            var tpl = []
+            tpl.push(v.seriesName + '<br>')
+            tpl.push(v.name + '：' + v.value[2])
+            return tpl.join('')
+          }
+        }
+        this.mapData = {
+          columns: [],
+          rows: []
+        }
+        this.chartSettings = {
+          mapOrigin: shenzhen,
+          position: 'shenzhen'
+        }
+        this.chartSeries = [{
+          name: 'pm2.5',
+          type: 'effectScatter', // 'scatter'
+          coordinateSystem: 'geo',
+          symbolSize: 10,
+          rippleEffect: {
+            brushType: 'stroke'
+          },
+          hoverAnimation: true,
+          data: [
+            { name: '', value: [this.longitude, this.latitude, 10] }
+          ],
+          label: {
+            normal: {
+              formatter: '{b}',
+              position: 'right',
+              show: true
+            },
+            emphasis: {
+              show: true
+            }
+          },
+          itemStyle: {
+            normal: {
+              color: '#DDDB3F'
+            }
+          }
+        }]
+        this.chartGeo = {
+          map: 'shenzhen',
+          label: {
+            emphasis: {
+              show: false
+            }
+          },
+          roam: true,
+          itemStyle: {
+            normal: {
+              areaColor: '#01A9DA',
+              borderColor: '#22A0CB'
+            },
+            emphasis: {
+              areaColor: '#0184B3'
+            }
+          }
+        }
+      })
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
